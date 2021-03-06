4c61c334bf8993ea4e58f125bd538046fd09e877
Beezxmaster <gmal48709@gmail.com>
2021-03-06 17:27:22 +0100

    Add Keypad

--- smarthome/smarthome.ino
+++ smarthome/smarthome.ino
@@ -1,21 +1,40 @@
 // initialize libraries
 #include <Servo.h>
+#include <Keypad.h>
 
+// Set a template to easy print operations
 template <typename T>
 Print& operator<<(Print& printer, T value) {
   printer.print(value);
   return printer;
 }
 
-// Pin Config
-const int soundpin = 7;
-const int ultraechopin = 6;
-const int ultratriggerpin = 5;
+const byte rows = 4;
+const byte cols = 4;
 
-// Variable Condig
+//define the cymbols on the buttons of the keypads
+char hexaKeys[rows][cols] = {
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+};
+
+// Pin Config
+const byte soundpin = 13;
+const byte ultraechopin = 6;
+const byte ultratriggerpin = 5;
+
+byte rowPins[rows] = {9, 8, 7, 6};
+byte colPins[cols] = {5, 4, 3, 2};
+
+
+// Variable Config
 int pin = 0;
 int len;
 
+//initialize an instance of class NewKeypad
+Keypad keypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, rows, cols);
 
 Servo servo; // initialize servo object
 
@@ -32,16 +51,21 @@ void playSound() {
 }
 
 void setup() {
+
+  /** 
+   * Pin Config
+  */
   pinMode(soundpin, OUTPUT);
-  playSound();
+  playSound(); // Give a soundoutput for starting
   
-  Serial.begin(9600);
-  Serial.flush();
+  Serial.begin(9600); // Start Serial Monitor on port 9600
+  Serial.flush(); // Waits for the transmission of outgoing serial data to complete
+  
   Serial << '\n' << "Smarthome is running now" << '\n';
   Serial << "Set the house password with four numbers" << '\n';
   while(Serial.available() == 0) {
     pin = Serial.read();
-    pin = Serial.parseInt();
+    pin = Serial.parseStr();
   }
   len = sizeof(pin);
   if(len == 4) {
@@ -54,5 +78,9 @@ void setup() {
 }
 
 void loop() {
-  
+  char key = keypad.getKey();
+
+  if(key) {
+    Serial << key;
+  }
 }
